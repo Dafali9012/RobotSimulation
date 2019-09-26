99c714656aadebbf31e40016c66237c09c64868b
David Glane <david@glane.net>
2019-09-26 09:34:40 +0200

    hej

--- src/Simulator.java
+++ src/Simulator.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 
 public class Simulator {
-    Scanner scan = new Scanner(System.in);
-    private int[ ] zebra ;
-    private int [] geopard;
+
+    private int [] zebra;
+    private int [] gepard;
 
     public void setZebra(int[] zebra) {
         this.zebra = zebra;
@@ -12,14 +12,15 @@ public class Simulator {
     public int[] getZebra() {
         return zebra;
     }
-    public void setGeopard(int[] geopard) {
-        this.geopard = geopard;
+    public void setGepard(int[] gepard) {
+        this.gepard = gepard;
     }
-    public int[] getGeopard() {
-        return geopard;
+    public int[] getGepard() {
+        return gepard;
     }
 
     public void inData() {
+        Scanner scan = new Scanner(System.in);
         System.out.println("Välja hur många zebra ");
 
     }
