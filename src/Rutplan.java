be814ccc7c02eba837d4b1aa79e22d424f385109
daniel.falklimberg <Daniel.falklimberg@gmail.com>
2019-09-26 11:26:38 +0200

    En massa saker.

--- src/Rutplan.java
+++ src/Rutplan.java
@@ -1,11 +1,27 @@
-public class Rutplan {
-    private int[][] rutplan;
+public abstract class Rutplan {
+    private String[][] rutplan;
+    private static int size;
 
     public Rutplan() {
-
+        size = 10;
+        rutplan = new String[size][size];
     }
 
-    public int getPos(int x, int y) {
-        return rutplan[x][y];
+    public void getPos(int x, int y) {
+        //return rutplan[x][y];
+    }
+
+    public static int getSize() {
+        return size;
+    }
+
+    public void print() {
+        for (int y = 0; y < 10; y++) {
+            for(int x = 0; x < 10; x++) {
+                rutplan[y][x] = "@  ";
+                System.out.print(rutplan[y][x]);
+                if(x == 9) System.out.print("\n");
+            }
+        }
     }
 }
--- src/Simulator.java
+++ src/Simulator.java
@@ -1,27 +1,91 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Simulator {
 
-    private int [] zebra;
-    private int [] gepard;
+    private Zebra[] zebror;
+    private Gepard[] geparder;
 
-    public void setZebra(int[] zebra) {
-        this.zebra = zebra;
-    }
+    public Simulator() {
 
-    public int[] getZebra() {
-        return zebra;
-    }
-    public void setGepard(int[] gepard) {
-        this.gepard = gepard;
-    }
-    public int[] getGepard() {
-        return gepard;
     }
 
-    public void inData() {
+    public void run() {
+        inputZebror();
+    }
+
+    public void setAntalZebra(int i) {
+        if (i < 1 || i > 10) {
+            inputZebror();
+            return;
+        }
+
+        zebror = new Zebra[i];
+        while (i > 0) {
+            Zebra z = new Zebra();
+            z.setPosX((int) (Math.random() * 10));
+            z.setPosY((int) (Math.random() * 10));
+            zebror[i - 1] = z;
+            System.out.print(zebror[i - 1].getPosX() + "  ");
+            System.out.println(zebror[i - 1].getPosY());
+            i--;
+        }
+    }
+
+    public int getzebror() {
+        return zebror.length;
+    }
+
+    public void setAntalGeparder(int i) {
+        if (i < 1 || i > 10) {
+            inputZebror();
+            return;
+        }
+
+        zebror = new Zebra[i];
+        while (i > 0) {
+            Zebra z = new Zebra();
+            z.setPosX((int) (Math.random() * 10));
+            z.setPosY((int) (Math.random() * 10));
+            zebror[i - 1] = z;
+            System.out.print(zebror[i - 1].getPosX() + "  ");
+            System.out.println(zebror[i - 1].getPosY());
+            i--;
+        }
+    }
+
+    public int getgeparder() {
+        return geparder.length;
+    }
+
+    public void inputZebror() {
+
+
+        System.out.print("Välj hur många zebror: ");
+        setAntalZebra(input());
+
+    }
+
+    public void inputGeparder() {
+        System.out.print("Välj hur många geparder: ");
+        setAntalGeparder(input());
+    }
+
+    public int input() {
+        boolean success = false; // Så länge success inte är true, då fortsätter loopen att loopa.
+        int input = 0;
         Scanner scan = new Scanner(System.in);
-        System.out.println("Välja hur många zebra ");
-
+        do {
+            try {
+                input = scan.nextInt();
+                success = true;
+            } catch (InputMismatchException e) {
+                System.out.print("Illegal input: ");
+                scan.nextLine();
+            }
+        }
+        while (!success);
+        scan.nextLine();
+        return input;
     }
 }
